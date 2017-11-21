799333c9703f37b7c32b81b91318e9ac66d5f93e
matlegdal <matlegdal@hotmail.com>
2017-11-21 17:28:50 -0500

    correct index to show a link to login instead of redirect auto

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 require_once 'pdo.php';
 session_start();
 
-if (!isset($_SESSION['user'])) {
-	header('Location: login.php');
-	return;
-}
+
 
 $autos = $pdo->query("SELECT * FROM autos");
 
@@ -28,32 +25,12 @@ if ( isset($_SESSION['success']) ) {
 <body>
 <h1>Welcome to the automobile database</h1>
 <?= $message ?>
-<table border="1">
-	<thead>
-		<tr>
-			<th>Make</th>
-			<th>Model</th>
-			<th>Year</th>
-			<th>Mileage</th>
-			<th>Action</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			while ($row = $autos->fetch(PDO::FETCH_ASSOC)) {
-				echo "<tr>";
-				echo "<td>" . htmlentities($row['make']) . "</td>";
-				echo "<td>" . htmlentities($row['model']) . "</td>";
-				echo "<td>" . htmlentities($row['year']) . "</td>";
-				echo "<td>" . htmlentities($row['mileage']) . "</td>";
-				echo "<td><a href='/'>Edit</a> / <a href='/'>Delete</a></td>";
-				echo "</tr>";
-			}
-		?>
-	</tbody>
-</table>
-<div><a href="add.php">Add a new entry</a></div>
-<div><a href="logout.php">Logout</a></div>
-
+<?php
+	if (!isset($_SESSION['user'])) {
+		echo "<a href='login.php'>Please login first</a>";
+	} else {
+		require 'table_autos.php';
+	}
+?>
 </body>
 </html>
---
+++ table_autos.php
@@ -0,0 +1,26 @@
+<table border="1">
+	<thead>
+		<tr>
+			<th>Make</th>
+			<th>Model</th>
+			<th>Year</th>
+			<th>Mileage</th>
+			<th>Action</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			while ($row = $autos->fetch(PDO::FETCH_ASSOC)) {
+				echo "<tr>";
+				echo "<td>" . htmlentities($row['make']) . "</td>";
+				echo "<td>" . htmlentities($row['model']) . "</td>";
+				echo "<td>" . htmlentities($row['year']) . "</td>";
+				echo "<td>" . htmlentities($row['mileage']) . "</td>";
+				echo "<td><a href='/'>Edit</a> / <a href='/'>Delete</a></td>";
+				echo "</tr>";
+			}
+		?>
+	</tbody>
+</table>
+<div><a href="add.php">Add a new entry</a></div>
+<div><a href="logout.php">Logout</a></div>
